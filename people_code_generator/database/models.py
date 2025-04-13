d2772a4fd6a1cb767922e0d0bec8f6dbe2873a77
cybersid123 <smaity@wgu.edu>
2025-04-13 18:42:04 +0530

    Update models.py

--- people_code_generator/database/models.py
+++ people_code_generator/database/models.py
@@ -1,1 +1,23 @@
+"""
+from sqlalchemy import Column, Integer, String, Date, Boolean, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+class Person(Base):
+    __tablename__ = 'people'
+    
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    date_of_birth = Column(Date, nullable=False)
+    email = Column(String(100), unique=True)
+    phone = Column(String(20))
+    address = Column(String(255))
+    unique_code = Column(String(50), unique=True)
+    photo = Column(LargeBinary)
+    is_active = Column(Boolean, default=True)
+    
+    def __repr__(self):
+        return f"<Person(id={self.id}, name='{self.first_name} {self.last_name}', code='{self.unique_code}')>"
+"""
